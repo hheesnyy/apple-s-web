b2bd2393535dfed6e31fd92596283102b747a119
hheesnyy <hhhhsoop@gmail.com>
2026-06-29 20:50:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-function showMood() {
+function showMood(mood) {
 
-    let mood =
-        document.getElementById("mood").value;
-
-    let result =
-        document.getElementById("result");
+    let result = document.getElementById("result");
 
     if (mood == "海") {
-
-        result.innerHTML =
-            "📍 假日之森<br>🎵 Space Song";
-
+        result.innerHTML = "📍 假日之森<br>🚶 看海發呆<br>🎵 Space Song";
     }
 
     else if (mood == "山") {
-
-        result.innerHTML =
-            "📍 侯硐貓村<br>🎵 Holocene";
-
+        result.innerHTML = "📍 侯硐貓村<br>🚶 散步<br>🎵 Holocene";
     }
 
     else if (mood == "city walk") {
-
-        result.innerHTML =
-            "📍 大稻埕<br>🎵 Apocalypse";
-
+        result.innerHTML = "📍 大稻埕<br>☕ 初二咖啡<br>🎵 Apocalypse";
     }
 
-    else {
+    else if (mood == "coffee date") {
+        result.innerHTML = "📍 果宅<br>☕ 早午餐<br>🎵 Best Part";
+    }
 
-        result.innerHTML =
-            "📍 北投<br>🎵 Ribs";
-
+    else if (mood == "nights") {
+        result.innerHTML = "📍 Sugarman<br>🌙 夜晚小酌<br>🎵 Nights";
     }
 
 }
