efa21da295316f08bb6dfb3f9c98f4fe3cab96de
hheesnyy <hhhhsoop@gmail.com>
2026-06-30 01:01:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
+let moodDatabase = [];
+
+fetch("moods.json")
+    .then(response => response.json())
+    .then(data => {
+
+        moodDatabase = data;
+
+        console.log("Database Loaded");
+
+    });
+
 function showMood(mood) {
 
-    let result = document.getElementById("result");
+    let result =
+        document.getElementById("result");
 
-    if (mood == "海") {
-        result.innerHTML = "📍 假日之森<br>🚶 看海發呆<br>🎵 Space Song";
-    }
+    let filteredData =
+        moodDatabase.filter(
+            item => item.mood === mood
+        );
 
-    else if (mood == "山") {
-        result.innerHTML = "📍 侯硐貓村<br>🚶 散步<br>🎵 Holocene";
-    }
+    let randomIndex =
+        Math.floor(
+            Math.random() * filteredData.length
+        );
 
-    else if (mood == "city walk") {
-        result.innerHTML = "📍 大稻埕<br>☕ 初二咖啡<br>🎵 Apocalypse";
-    }
+    let recommendation =
+        filteredData[randomIndex];
 
-    else if (mood == "coffee date") {
-        result.innerHTML = "📍 果宅<br>☕ 早午餐<br>🎵 Best Part";
-    }
+    result.innerHTML =
 
-    else if (mood == "nights") {
-        result.innerHTML = "📍 Sugarman<br>🌙 夜晚小酌<br>🎵 Nights";
-    }
+        `
+        <div class="result-card">
 
+            <p>📍 ${recommendation.place}</p>
+
+            <p>🚶 ${recommendation.activity}</p>
+
+            <p>🎵 ${recommendation.song}</p>
+
+        </div>
+        `;
 }
